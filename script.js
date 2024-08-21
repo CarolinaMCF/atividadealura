e471a02943ec9f9092774f83d0c539028f528a69
Carolina Manhães <105887881+CarolinaMCF@users.noreply.github.com>
2024-08-21 07:43:08 -0300

    .

---
+++ script.js
@@ -0,0 +1,47 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaPrincipal = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('.texto-resultado');
+
+const alternativas = [
+    {
+        enunciado: "No âmbito social, você prefere:",
+        alternativa: [
+            {
+                texto: "Alternativa 1",
+                afirmacao: "Afirmação 1",
+            },
+            {
+                texto: "Alternativa 2",
+                afirmacao: "Afirmação 2",
+            },
+        ]
+    },
+    {
+        enunciado: "No âmbito ambiental, você prefere:",
+        alternativa: [
+            {
+                texto: "Alternativa 3",
+                afirmacao: "Afirmação 3",
+            },
+            {
+                texto: "Alternativa 4",
+                afirmacao: "Afirmação 4",
+            },
+        ]
+    },
+    {
+        enunciado: "No âmbito tecnológico, você prefere:",
+        alternativa: [
+            {
+                texto: "Alternativa 5",
+                afirmacao: "Afirmação 5",
+            },
+            {
+                texto: "Alternativa 6",
+                afirmacao: "Afirmação 6",
+            },
+        ]
+    },
+]
